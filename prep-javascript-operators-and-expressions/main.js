35f8494957663fec2140e83422b985781fb791de
Kyle Spence <kyle.w.spence@gmail.com>
2025-01-16 20:55:43 +0000

    Completed prep-javascript-operators-and-expressions exercise.

--- prep-javascript-operators-and-expressions/main.js
+++ prep-javascript-operators-and-expressions/main.js
@@ -1,38 +1,39 @@
-let number1 = 0;
-let number2 = 1;
+const number1 = 0;
+const number2 = 1;
 const product = number1 * number2;
 console.log('Product: ' + product);
 console.log('typeof for product is: ' + typeof product);
 
-let charge = 5;
-let payment = 1;
-let amountRemaining = payment - charge;
+const charge = 5;
+const payment = 1;
+const amountRemaining = payment - charge;
 console.log('Amount remaining: ' + amountRemaining);
 console.log('typeof for amountRemaining is: ' + typeof amountRemaining);
 
-let tests = 5;
-let assignments = 10;
-let final = 1;
-let grade = (tests + assignments + final) / 3;
+const tests = 5;
+const assignments = 10;
+const final = 1;
+const grade = (tests + assignments + final) / 3;
 console.log('Grade: ' + grade);
 console.log('typeof for grade is: ' + typeof grade);
 
-let firstName = 'Kyle';
-let lastName = 'Spence';
-let fullName = firstName + ' ' + lastName;
+const firstName = 'Kyle';
+const lastName = 'Spence';
+const fullName = firstName + ' ' + lastName;
 console.log('My name is ' + fullName);
 console.log('typeof for fullName is: ' + typeof fullName);
 
-let pH = 15;
-let isAcidic = pH < 7;
+const pH = 15;
+const isAcidic = pH < 7;
 console.log('Is it acidic: ' + isAcidic);
 console.log('typeof for isAcidic is ' + typeof isAcidic);
 
-let numberOfSoldiers = 1000;
-let isSparta = numberOfSoldiers === 300;
+const numberOfSoldiers = 1000;
+const isSparta = numberOfSoldiers === 300;
 console.log('Are we cooler than Sparta at Thermopylae: ' + isSparta);
 console.log('typeof for isSparta is: ' + typeof isSparta);
 
-let nickname = fullName + ' is the GOAT!';
+let nickname = fullName;
+nickname += ' is the GOAT!';
 console.log('Some people say that ' + nickname);
 console.log('typeof for nickname is: ' + typeof nickname);
